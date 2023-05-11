191de0d322de286e3e25f8c6801408ce3e784480
none2004 <noe2811@gmx.fr>
2023-05-11 14:40:05 +0200

    Début

---
+++ Presque_Perlin.py
@@ -0,0 +1,13 @@
+import random
+class Tableau2d:
+    def __init__:
+        self.Longueur,Largeur = 16,16
+        self.Carte = [[Chunck(self.Longueur,self.Largeur) for _ in range(Longueur)] for _ in range(Largeur)]
+
+
+class Chunck:
+    def __init__(Longeur,Largeur):
+        self.Longueur,Largeur = Longeur,Largeur
+        self.point = random.randint(0,255) #point de chaque côté du chunck
+        self.chunck = []#plan 2d de longeur/largeur 
+print(Tableau)
