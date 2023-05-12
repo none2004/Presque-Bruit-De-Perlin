ef5e672a5d57e36fb8226ce8f4f2da45f1779392
none2004 <noe2811@gmx.fr>
2023-05-12 17:43:04 +0200

    Mise en place des "chuncks"

--- Presque_Perlin.py
+++ Presque_Perlin.py
@@ -1,13 +1,66 @@
 import random
-class Tableau2d:
-    def __init__:
-        self.Longueur,Largeur = 16,16
-        self.Carte = [[Chunck(self.Longueur,self.Largeur) for _ in range(Longueur)] for _ in range(Largeur)]
+from PIL import Image
+class Perlin:
+    def __init__(self,Longueur,Largeur,file):
+        self.Tableau = Image.new("L", (Longueur*16,Largeur*16), color=0)
+        self.file = file
+
+    def ChangePixel(self,xy,value):
+        '''Change la couleur du pixel(0 à 255) au coordonné xy(tuple)'''
+        self.Tableau.putpixel(xy,value)
+
+    def Polissage(self,xy1,xy2):
+        '''Fait une variation de couleur de xy1 à xy2 que si les points sont alligniés. xy1<xy2'''#faire quand la différence de couleur et plus petite que la différence distance
+        if xy1[1] == xy2[1] and xy1[0]!=xy2[0]:
+            diff = abs(xy1[0]-xy2[0])
+            diffColor = abs(self.Tableau.getpixel(xy1)-self.Tableau.getpixel(xy2))//diff
+            print(self.Tableau.getpixel(xy1),self.Tableau.getpixel(xy2))
+            if self.Tableau.getpixel(xy1) > self.Tableau.getpixel(xy2):
+                for d in range(1,diff):
+                    self.ChangePixel((xy1[0]+d,xy1[1]),(self.Tableau.getpixel(xy1)+diffColor*d))
+                    print((xy1[0]+d,xy1[1]),(self.Tableau.getpixel(xy1)-diffColor*d))
+            else:
+                for d in range(1,diff):
+                    self.ChangePixel((xy1[0]+d,xy1[1]),(self.Tableau.getpixel(xy1)+diffColor*d))
+                    print((xy1[0]+d,xy1[1]),(self.Tableau.getpixel(xy1)+diffColor*d))
+                
+
+        if xy1[1] != xy2[1] and xy1[0]==xy2[0]:
+            diff = abs(xy1[1]-xy2[1])
+            diffColor = abs(self.Tableau.getpixel(xy1)-self.Tableau.getpixel(xy2))//diff
+            print(self.Tableau.getpixel(xy1),self.Tableau.getpixel(xy2))
+            if self.Tableau.getpixel(xy1) > self.Tableau.getpixel(xy2):
+                for d in range(1,diff):
+                    self.ChangePixel((xy1[0],xy1[1]+d),(self.Tableau.getpixel(xy1)+diffColor*d))
+                    print((xy1[0],xy1[1]+d),(self.Tableau.getpixel(xy1)-diffColor*d))
+            else:
+                for d in range(1,diff):
+                    self.ChangePixel((xy1[0],xy1[1]+d),(self.Tableau.getpixel(xy1)+diffColor*d))
+                    print((xy1[0],xy1[1]+d),(self.Tableau.getpixel(xy1)+diffColor*d))
+            
+    def Point(self,distance):
+        '''Pose les points à distance'''
+        for x in range(0,self.Tableau.height,distance):
+            for y in range(0,self.Tableau.width,distance):
+                self.ChangePixel((x,y),random.randint(0,255))
+        self.PolissagePoint(distance)
+    def PolissagePoint(self,distance):
+        '''Utilise la fonction polissage toute les distances'''
+        for x in range(0,self.Tableau.height-distance,distance):
+            for y in range(0,self.Tableau.width-distance,distance):
+                print(x,y)
+                self.Polissage((x,y),(x+distance,y))
+                self.Polissage((x,y),(x,y+distance))
+                
+    def save(self):
+        '''sauvegarde dans le fichier où est le programme en .PNG'''
+        self.Tableau.save(self.file+".png")
 
 
-class Chunck:
-    def __init__(Longeur,Largeur):
-        self.Longueur,Largeur = Longeur,Largeur
-        self.point = random.randint(0,255) #point de chaque côté du chunck
-        self.chunck = []#plan 2d de longeur/largeur 
-print(Tableau)
+Perlin = Perlin(16,16,"Perlin")
+##Perlin.ChangePixel((15,45),245)
+##Perlin.ChangePixel((30,45),60)
+##Perlin.Polissage((15,45),(30,45))
+Perlin.Point(15)
+
+Perlin.save()
--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # Presque Bruit De Perlin
  Essayer d'approcher le bruit de perlin en python
-C'est un essais sur le bruit de perlin : False
-L'idée du code
-plan 2d avec un point tout les 16 pixels,
-sur chaque point un vecteur de 0 à 255 avec du pseudo aléatoire, qui commence de en haut à gauche et fini en bas à droite : False
-entre les points il y a un polissage des valeurs : False
-puis je fini par une mise en noir et blanc les valeurs : False
-Test pour ajouter une seed
+C'est un essais sur le bruit de perlin
